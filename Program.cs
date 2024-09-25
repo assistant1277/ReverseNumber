20593a3aa22a7a8944e51219c2f4d7068da8d376
assistant1277 <shivarajghangale2002@gmail.com>
2024-09-25 14:46:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ReverseNumber.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number to reverse ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Reverse.CheckAndPrintReverse(inputNumber);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ model/Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber.model
+{
+    public class Reverse
+    {
+        public static int ReverseDigits(int number)
+        {
+            int reversedNumber = 0;
+            while(number > 0)
+            {
+                int lastDigit = number % 10;
+                reversedNumber = (reversedNumber * 10) + lastDigit;
+                number /= 10;
+            }
+
+            return reversedNumber;
+        }
+        public static void CheckAndPrintReverse(int number)
+        {
+            int reversed = ReverseDigits(number);
+            Console.WriteLine($"reverse of number {number} is {reversed}");
+        }
+    }
+}
